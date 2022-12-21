8cd78b5b6b343aceca0dab3fb2532ede2adc139e
DenisaNicoletaH <denisanhategan@gmail.com>
2022-12-21 15:38:48 -0500

    Initial commit

---
+++ src/main/java/com/example/journallistfinaljavaweb/JournalListFinalJavaWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.journallistfinaljavaweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JournalListFinalJavaWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JournalListFinalJavaWebApplication.class, args);
+    }
+
+}
